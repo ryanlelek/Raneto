4197b6439e724fa1c283a7a96e1e61e3c4e500ee
cagegong <cagegong@gmail.com>
2015-08-25 13:53:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 
 	// Your site title (format: page_title - site_title)
-	site_title: 'Raneto Docs',
+	site_title: '顺顺文档',
 
 	// The base URL of your site (can use %base_url% in Markdown files)
 	base_url: '',
