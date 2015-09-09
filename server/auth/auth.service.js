935374eb0ad2ec730f188c16e741dfceea8ebf44
noodleswww <boatwww@icloud.com>
2015-09-09 14:12:58 +0800

    update email user, remove wirteHeader

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -5,7 +5,7 @@ var config = require('../config');
 exports.loginRequired = function (req, res, next) {
   //delete req.session.session_user;
   if (!req.session || !req.session.session_user) {
-    var msg = '<html><head><title>没有登陆</title></head><body><p>还没登陆，3秒自动跳转，也可以点我！<a href=/auth/login">登陆</a></p><script>setInterval(function(){location.href="/auth/login"},3000);</script></body></html>';
+    var msg = '<html><head><title>没有登录</title></head><body><script>setTimeout(function(){location.href="/auth/login"},0);</script></body></html>';
     return res.status(403).send(msg);
   } else {
     next();
--- tools/mail.js
+++ tools/mail.js
@@ -4,7 +4,7 @@ var log4js = require('./logger');
 var util = require('util');
 
 var transport = mailer.createTransport(config.mail_opts);
-var ROOT_URL = 'http://' + config.host;
+var ROOT_URL = config.host;
 
 var sendMail = function (mailObj) {
   transport.sendMail(mailObj, function (err,info) {
@@ -22,7 +22,7 @@ exports.sendMail = sendMail;
 exports.sendActiveMail = function (to, token, name) {
   var from = util.format('%s<%s>', config.site_title, config.mail_opts.auth.user);
   var sub = config.site_title + '用户激活';
-  var html = '<p>您好：' + name + '</p><p>收到您在' + config.site_title + '注册信息，请点击下面的链接来激活帐户：</p>' +
+  var html = '<p>您好：' + to + '</p><p>收到您在' + config.site_title + '注册信息，请点击下面的链接来激活帐户：</p>' +
     '<a href="' + ROOT_URL + '/auth/active_account?key=' + token + '&name=' + name + '">激活链接</a>';
 
   exports.sendMail({
@@ -36,7 +36,7 @@ exports.sendActiveMail = function (to, token, name) {
 exports.sendResetPassMail = function (to, token, name) {
   var from = util.format('%s<%s>', config.site_title, config.mail_opts.auth.user);
   var sub = config.site_title + '网站用户密码重置';
-  var html = '<p>您好：' + name + '</p>' +
+  var html = '<p>您好：' + to + '</p>' +
     '<p>收到您在' + config.site_title + '重置密码的请求，请在24小时内单击下面的链接来重置密码：</p>' +
     '<a href="' + ROOT_URL + '/auth/reset_pass?key=' + token + '&name=' + name + '">重置密码链接</a>';
 
