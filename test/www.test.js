f6af28bc4bae8e52c750679162a46b5743ad7672
kaiqigong <cagegong@gmail.com>
2015-09-12 15:43:27 +0800

    add info

--- test/www.test.js
+++ test/www.test.js
@@ -5,6 +5,7 @@ var config = require('../server/config');
 describe('test/www.test.js', function () {
   it('should / status 302', function (done) {
     request(app).get('/').end(function (err, res) {
+      console.log(res);
       res.status.should.equal(302);
       res.text.should.containEql(config.site_title);
       done();
