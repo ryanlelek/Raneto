79a72353c30b0fda834d9415cf344aea4c091c05
kaiqigong <cagegong@gmail.com>
2015-09-12 15:57:08 +0800

    include should in dev; refine test

--- test/www.test.js
+++ test/www.test.js
@@ -3,11 +3,9 @@ var app = require('../server');
 var config = require('../server/config');
 
 describe('test/www.test.js', function () {
-  it('should / status 302', function (done) {
+  it('request should return status 403 before login', function (done) {
     request(app).get('/').end(function (err, res) {
-      console.log(res);
-      res.status.should.equal(302);
-      res.text.should.containEql(config.site_title);
+      res.status.should.equal(403);
       done();
     });
   });
