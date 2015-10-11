430e4f8f50ee521681e65c4ebc774e0bf4418e5a
Ryan Lelek <ryanlelek@gmail.com>
2015-10-10 21:58:43 -0700

    Cleanup: Delinting server-side JavaScript

--- config.js
+++ config.js
@@ -1,44 +1,46 @@
-var config = {
 
-	// Your site title (format: page_title - site_title)
-	site_title: 'Raneto Docs',
+'use strict';
 
-	// The base URL of your site (can use %base_url% in Markdown files)
-	base_url: '',
+// Exports
+module.exports = {
 
-	// Used for the "Get in touch" page footer link
-	support_email: '',
+  // Your site title (format: page_title - site_title)
+  site_title: 'Raneto Docs',
 
-	// Footer copyright content
-	copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Powered by Raneto</a>',
+  // The base URL of your site (can use %base_url% in Markdown files)
+  base_url: '',
 
-	// Excerpt length (used in search)
-	excerpt_length: 400,
+  // Used for the "Get in touch" page footer link
+  support_email: '',
 
-	// The meta value by which to sort pages (value should be an integer)
-	// If this option is blank pages will be sorted alphabetically
-	page_sort_meta: 'sort',
+  // Footer copyright content
+  copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Powered by Raneto</a>',
 
-	// Should categories be sorted numerically (true) or alphabetically (false)
-	// If true category folders need to contain a "sort" file with an integer value
-	category_sort: true,
+  // Excerpt length (used in search)
+  excerpt_length: 400,
 
-	// The base URL of your images folder (can use %image_url% in Markdown files)
-	image_url: '/images',
+  // The meta value by which to sort pages (value should be an integer)
+  // If this option is blank pages will be sorted alphabetically
+  page_sort_meta: 'sort',
 
-	// Specify the path of your content folder where all your '.md' files are located
-	content_dir: './content/',
+  // Should categories be sorted numerically (true) or alphabetically (false)
+  // If true category folders need to contain a "sort" file with an integer value
+  category_sort: true,
 
-	// Add your analytics tracking code (including script tags)
-	analytics: "",
+  // The base URL of your images folder (can use %image_url% in Markdown files)
+  image_url: '/images',
 
-	// Set to true to enable authentication
-	authentication: false,
+  // Specify the path of your content folder where all your '.md' files are located
+  content_dir: './content/',
 
-	credentials: {
-		username: 'admin',
-		password: 'password'
-	}
+  // Add your analytics tracking code (including script tags)
+  analytics: '',
+
+  // Set to true to enable HTTP Basic authentication
+  authentication : false,
+  credentials    : {
+    username : 'admin',
+    password : 'password'
+  }
+
 };
-
-module.exports = config;
