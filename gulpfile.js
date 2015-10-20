94c0ed6053b4eaa0809ada52cdc75709eb046723
Stephen Bussard <me@stephenbussard.com>
2015-10-19 21:59:49 -0500

    Module: replacing bower components with npm, adding gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('default', ['copy_libs']);
+
+gulp.task('copy_libs', function() {
+  var source = [
+    'node_modules/bootstrap/**/*',
+    'node_modules/fitvids/**/*',
+    'node_modules/highlightjs/**/*',
+    'node_modules/jquery/**/*',
+    'node_modules/masonry-layout/**/*',
+  ];
+
+  var dest = 'public/lib';
+
+  var stream = gulp.src(source, { base: 'node_modules' })
+    .pipe(gulp.dest(dest));
+
+  return stream;
+});
