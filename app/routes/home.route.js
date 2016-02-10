a513e98db062f205bd633a70e61b5e4a8ed61358
Ryan Lelek <ryanlelek@gmail.com>
2016-02-10 02:04:57 -0500

    Refactor: Converting home page to use get_filepath()

--- app/routes/home.route.js
+++ app/routes/home.route.js
@@ -2,29 +2,39 @@
 'use strict';
 
 // Modules
-var path                           = require('path');
 var fs                             = require('fs');
 var moment                         = require('moment');
+var get_filepath                   = require('../functions/get_filepath.js');
 var remove_image_content_directory = require('../functions/remove_image_content_directory.js');
 
 function route_home (config, raneto) {
   return function (req, res, next) {
 
-    var suffix = 'edit';
-    var slug = '/index';
+    // Generate Filepath
+    var filepath = get_filepath({
+      content  : raneto.config.content_dir,
+      filename : 'index'
+    });
 
-    var pageList     = remove_image_content_directory(config, raneto.getPages(slug));
-    var filePath     = path.normalize(raneto.config.content_dir + slug);
-
-    if (fs.existsSync(filePath + '.md')) {
+    // Do we have an index.md file?
+    // If so, use that.
+    if (fs.existsSync(filepath + '.md')) {
       return next();
     }
 
-    if (filePath.indexOf(suffix, filePath.length - suffix.length) !== -1) {
-      filePath = filePath.slice(0, - suffix.length - 1);
+    // Otherwise, we're generating the home page listing
+    var suffix = 'edit';
+    if (filepath.indexOf(suffix, filepath.length - suffix.length) !== -1) {
+      filepath = filepath.slice(0, - suffix.length - 1);
     }
 
-    var stat = fs.lstatSync(path.join(config.theme_dir, config.theme_name, 'templates', 'home.html'));
+    var template_filepath = get_filepath({
+      content  : [config.theme_dir, config.theme_name, 'templates'].join('/'),
+      filename : 'home.html'
+    });
+
+    var stat     = fs.lstatSync(template_filepath);
+    var pageList = remove_image_content_directory(config, raneto.getPages('/index'));
 
     return res.render('home', {
       config        : config,
