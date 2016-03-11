d7563f7957044f686e4373cfebb01f7cdb4b55ea
Roger Hutchings <roger@hutchin.gs>
2016-03-11 12:25:42 +0000

    Add editorconfig, convert all tabs to spaces

--- app/functions/create_meta_info.js
+++ app/functions/create_meta_info.js
@@ -3,24 +3,24 @@
 
 // Returns an empty string if all input strings are empty
 function create_meta_info(meta_title, meta_description, meta_sort) {
-	var newline = '\n';
-	var comment_start = '/*';
-	var comment_end = '*/';
-	var title_label = 'Title: ';
-	var description_label = 'Description: ';
-	var description_sort = 'Sort: ';
+  var newline = '\n';
+  var comment_start = '/*';
+  var comment_end = '*/';
+  var title_label = 'Title: ';
+  var description_label = 'Description: ';
+  var description_sort = 'Sort: ';
 
-	var no_meta_info_is_present = !meta_title && !meta_description && !meta_sort;
-	var meta_header = '';
-	if (!no_meta_info_is_present) {
-		meta_header = comment_start + newline +
-		(meta_title ? title_label + meta_title + newline : '') +
-		(meta_description ? description_label + meta_description + newline : '') +
-		(meta_sort ? description_sort + meta_sort + newline : '') +
-		comment_end + newline + newline;
-	}
+  var no_meta_info_is_present = !meta_title && !meta_description && !meta_sort;
+  var meta_header = '';
+  if (!no_meta_info_is_present) {
+    meta_header = comment_start + newline +
+    (meta_title ? title_label + meta_title + newline : '') +
+    (meta_description ? description_label + meta_description + newline : '') +
+    (meta_sort ? description_sort + meta_sort + newline : '') +
+    comment_end + newline + newline;
+  }
 
-	return meta_header;
+  return meta_header;
 }
 
 // Exports
