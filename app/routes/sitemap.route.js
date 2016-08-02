af3499a9c7cfcf7573771658bea8d3f590767dd2
Sebastien Foubert <sfoubert.ext@galerieslafayette.com>
2016-08-02 16:34:29 +0200

    add dynamic sitemap.xml with lastmod

--- app/routes/sitemap.route.js
+++ app/routes/sitemap.route.js
@@ -1,26 +1,31 @@
 'use strict';
 
 // Modules
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
-var sm = require('sitemap');
+var url    = require('url');
+var path   = require('path');
+var fs     = require('fs');
+var sm     = require('sitemap');
+var _      = require("underscore");
+var moment = require("moment");
 
 function route_sitemap(config, raneto) {
   return function (req, res, next) {
 
     var hostname = req.headers.host;
-
     var content_dir = path.normalize(raneto.config.content_dir);
 
     // get list md files
-    var files = listFiles(content_dir, '.md');
-    files = files.map(function (file) {
+    var files = listFiles(content_dir);
+    files = _.filter(files, function (file) {
+      return file.endsWith('.md');
+    });
+
+    var filesPath = files.map(function (file) {
       return file.replace(content_dir, '');
     });
 
     // generate list urls
-    var urls = files.map(function (file) {
+    var urls = filesPath.map(function (file) {
       return '/' + file.replace('.md', '').replace('\\', '/');
     });
 
@@ -31,10 +36,12 @@ function route_sitemap(config, raneto) {
     });
 
     for (var i = 0, len = urls.length; i < len; i++) {
+      var stat = fs.lstatSync(files[i]);
       sitemap.add({
         url: urls[i],
-        changefreq : 'weekly',
-        priority : 0.8
+        changefreq: 'weekly',
+        priority: 0.8,
+        lastmod: moment(stat.mtime).format('YYYY-MM-DD')
       });
     }
 
@@ -44,7 +51,7 @@ function route_sitemap(config, raneto) {
   };
 }
 
-function listFiles(dir, suffix) {
+function listFiles(dir) {
   return fs.readdirSync(dir).reduce(function (list, file) {
     var name = path.join(dir, file);
     var isDir = fs.statSync(name).isDirectory();
