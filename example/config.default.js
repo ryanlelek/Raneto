26deae3d8710a5887838b80d5e49674a2521162e
Ryan Lelek <ryanlelek@gmail.com>
2016-09-12 18:23:40 -0400

    Example: Default configuration edits

--- example/config.default.js
+++ example/config.default.js
@@ -50,13 +50,13 @@ var config = {
   allow_editing : true,
 
   // Set to true to enable HTTP Basic Authentication
-  authentication : false,
+  authentication : true,
 
   // If editing is enabled, set this to true to only authenticate for editing, not for viewing
   authentication_for_edit: true,
 
   // Google OAuth
-  googleoauth: true,
+  googleoauth: false,
   oauth2 : {
     client_id: 'GOOGLE_CLIENT_ID',
     client_secret: 'GOOGLE_CLIENT_SECRET',
