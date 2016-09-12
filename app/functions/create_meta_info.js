584b4772826bfba20bc96cb0c937464b88845719
Ryan Lelek <ryanlelek@gmail.com>
2016-09-12 18:04:49 -0400

    Delinting

--- app/functions/create_meta_info.js
+++ app/functions/create_meta_info.js
@@ -5,19 +5,19 @@
 var yaml = require('js-yaml');
 
 // Returns an empty string if all input strings are empty
-function create_meta_info(meta_title, meta_description, meta_sort) {
+function create_meta_info (meta_title, meta_description, meta_sort) {
 
   var yamlDocument = {};
   var meta_info_is_present = meta_title && meta_description && meta_sort;
 
   if (meta_info_is_present) {
-    if (meta_title) yamlDocument["Title"] = meta_title;
-    if (meta_description) yamlDocument["Description"] = meta_description;
-    if (meta_sort) yamlDocument["Sort"] = parseInt(meta_sort);
+    if (meta_title)       { yamlDocument.Title       = meta_title;              }
+    if (meta_description) { yamlDocument.Description = meta_description;        }
+    if (meta_sort)        { yamlDocument.Sort        = parseInt(meta_sort, 10); }
 
-    return yaml.safeDump(yamlDocument) + "---\n";
+    return yaml.safeDump(yamlDocument) + '---\n';
   } else {
-    return "";
+    return '';
   }
 
 }
