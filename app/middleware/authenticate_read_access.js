e04939b91e838cc82580e8337f803afc58179d37
Ryan Lelek <ryanlelek@gmail.com>
2017-01-15 20:32:15 -0500

    Delinting

--- app/middleware/authenticate_read_access.js
+++ app/middleware/authenticate_read_access.js
@@ -1,31 +1,29 @@
 'use strict';
 
-function middleware_authenticate_read_access(config) {
-    if (config.authentication === true  &&
-		config.authentication_for_edit === false) {
-		
-		return function(req, res, next) {
-			if (!req.session.loggedIn) {
-				if (req.path === '/rn-login' ||
-				    req.path === '/logout'   ||
-				    req.path === '/login') {
-				    return next();
-				} else {
-				  	res.redirect(403, '/login');
-				  	return;
-				}
-				return next();
-			} else {
-				return next();
-			};
-		}
-    } else {
+function middleware_authenticate_read_access (config) {
 
-        // No Authentication Required
-        return function(req, res, next) {
-            return next();
-        };
-    }
+  if (config.authentication === true && config.authentication_for_edit === false) {
+    return function (req, res, next) {
+      if (!req.session.loggedIn) {
+        if (req.path === '/rn-login' ||
+            req.path === '/logout'   ||
+            req.path === '/login') {
+          return next();
+        } else {
+          res.redirect(403, '/login');
+          return;
+        }
+        return next();
+      } else {
+        return next();
+      }
+    };
+  } else {
+    // No Authentication Required
+    return function (req, res, next) {
+      return next();
+    };
+  }
 }
 
 // Exports
