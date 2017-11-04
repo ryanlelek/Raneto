9911c39e7c4c6042e3ebedb247d3b43392424e79
Mandeep <mandeepw@nulogy.com>
2017-11-04 14:26:42 -0400

    Update site title

--- example/config.default.js
+++ example/config.default.js
@@ -4,7 +4,7 @@
 var config = {
 
   // Your site title (format: page_title - site_title)
-  site_title: 'Raneto Docs',
+  site_title: 'Autism Ontario',
 
   // The base URL of your site (can use %base_url% in Markdown files)
   base_url: '',
