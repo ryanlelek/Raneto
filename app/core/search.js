d098fe71174daa4fda5062bc9e39b8aaa01eacaa
FilunK <yishikawa@koromotani.sakura.ne.jp>
2020-09-07 15:19:12 +0900

    use lunr-languages/tinyseg instead of tiny-segmenter

--- app/core/search.js
+++ app/core/search.js
@@ -1,69 +1,68 @@
-
 'use strict';
 
-const path              = require('path');
+const path = require('path');
 const contentProcessors = require('../functions/contentProcessors');
-const utils             = require('./utils');
-const pageHandler       = require('./page');
+const utils = require('./utils');
+const pageHandler = require('./page');
 
 let instance = null;
 let stemmers = null;
 
-function getLunr (config) {
-  if (instance === null) {
-    instance = require('lunr');
-    instance.TinySegmenter = require('tiny-segmenter');
-    require('lunr-languages/lunr.stemmer.support')(instance);
-    require('lunr-languages/lunr.multi')(instance);
-    config.searchExtraLanguages.forEach(lang =>
-      require('lunr-languages/lunr.' + lang)(instance)
+function getLunr(config) {
+    if (instance === null) {
+        instance = require('lunr');
+        require('lunr-languages/lunr.stemmer.support')(instance);
+        require('lunr-languages/lunr.multi')(instance);
+        require('lunr-languages/tinyseg')(instance);
+        config.searchExtraLanguages.forEach(lang =>
+            require('lunr-languages/lunr.' + lang)(instance)
+        );
+    }
+    return instance;
+}
+
+function getStemmers(config) {
+    if (stemmers === null) {
+        const languages = ['en'].concat(config.searchExtraLanguages);
+        stemmers = getLunr(config).multiLanguage.apply(null, languages);
+    }
+    return stemmers;
+}
+
+async function handler(query, config) {
+    const contentDir = utils.normalizeDir(path.normalize(config.content_dir));
+    const rawDocuments = await utils.promiseGlob(contentDir + '**/*.md');
+    const potentialDocuments = await Promise.all(
+        rawDocuments.map(filePath => contentProcessors.extractDocument(
+            contentDir, filePath, config.debug
+        ))
     );
-  }
-  return instance;
+    const documents = potentialDocuments
+        .filter(doc => doc !== null);
+
+    const lunrInstance = getLunr(config);
+    const idx = lunrInstance(function() {
+        this.use(getStemmers(config));
+        this.field('title');
+        this.field('body');
+        this.ref('id');
+        documents.forEach((doc) => this.add(doc), this);
+    });
+
+    const results = idx.search(query);
+
+    const searchResults = await Promise.all(
+        results.map(result => processSearchResult(contentDir, config, query, result))
+    );
+
+    return searchResults;
 }
 
-function getStemmers (config) {
-  if (stemmers === null) {
-    const languages = ['en'].concat(config.searchExtraLanguages);
-    stemmers = getLunr(config).multiLanguage.apply(null, languages);
-  }
-  return stemmers;
-}
+async function processSearchResult(contentDir, config, query, result) {
+    const page = await pageHandler(contentDir + result.ref, config);
+    page.excerpt = page.excerpt.replace(new RegExp('(' + query + ')', 'gim'), '<span class="search-query">$1</span>');
 
-async function handler (query, config) {
-  const contentDir = utils.normalizeDir(path.normalize(config.content_dir));
-  const rawDocuments = await utils.promiseGlob(contentDir + '**/*.md');
-  const potentialDocuments = await Promise.all(
-    rawDocuments.map(filePath => contentProcessors.extractDocument(
-      contentDir, filePath, config.debug
-    ))
-  );
-  const documents = potentialDocuments
-    .filter(doc => doc !== null);
-
-  const lunrInstance = getLunr(config);
-  const idx = lunrInstance(function () {
-    this.use(getStemmers(config));
-    this.field('title');
-    this.field('body');
-    this.ref('id');
-    documents.forEach((doc) => this.add(doc), this);
-  });
-
-  const results = idx.search(query);
-
-  const searchResults = await Promise.all(
-    results.map(result => processSearchResult(contentDir, config, query, result))
-  );
-
-  return searchResults;
-}
-
-async function processSearchResult (contentDir, config, query, result) {
-  const page = await pageHandler(contentDir + result.ref, config);
-  page.excerpt = page.excerpt.replace(new RegExp('(' + query + ')', 'gim'), '<span class="search-query">$1</span>');
-
-  return page;
+    return page;
 }
 
 exports.default = handler;
